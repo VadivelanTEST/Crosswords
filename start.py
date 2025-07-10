f2ba67cacb55679bdd56525d0137445dd059fe94
VadivelanTEST <94745561+VadivelanTEST@users.noreply.github.com>
2025-07-10 08:40:56 +0530

    Update start.py

--- start.py
+++ start.py
@@ -423,7 +423,7 @@ def main():
     if crossword_data:
         date_str = crossword_data['date']
         date = datetime.strptime(date_str, '%m/%d/%Y')
-        title = f"XWordHint Crossword Answers Hints & Solutions - {date.strftime('%B %d, %Y')} | Expert Puzzle Help"
+        title = f"XWordHint Crossword Answers {date.strftime('%B %d, %Y')} Hints $ Solutions | Expert Puzzle Help"
         formatted_html = format_to_html(crossword_data, date)
         send_email("velanms1993.qrco@blogger.com", title, formatted_html)
         print(f"✅ Email sent successfully for {date.strftime('%B %d, %Y')} crossword!")
